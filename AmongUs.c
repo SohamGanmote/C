4a5f0a2fe8ef968a2dafb3a0823c1495e637ceee
SohamGanmote <77290684+SohamGanmote@users.noreply.github.com>
2021-05-08 22:59:26 +0530

    Add files via upload

---
+++ AmongUs.c
@@ -0,0 +1,142 @@
+#include<stdio.h>
+int main()
+{   
+    printf("\033[0;32mX X X X X X X X X X X X X X X X\n"); //Green Colour
+    
+    printf("\033[0;32mX X X X X X"); //Green Colour
+    printf("\033[0;30m X X X X X X"); //Black Colour
+    printf("\033[0;32m X X X X\n"); //Green Colour
+
+    printf("\033[0;32mX X X X X");//Green Colour
+    printf("\033[0;30m X"); //Black Colour
+    printf("\033[0;33m X X X X X X"); //Yellow Colour
+    printf("\033[0;30m X"); //Black Colour
+    printf("\033[0;32m X X X\n"); //Green Colour
+
+    printf("\033[0;32mX X X X");//Green Colour
+    printf("\033[0;30m X ");//Black Colour
+    printf("\033[0;33mX X X X X X X X X");//Yellow Colour
+    printf("\033[0;32m X X\n");//Green Colour
+
+    printf("\033[0;32mX X X");//Green Colour
+    printf("\033[0;30m X");//Black Colour
+    printf("\033[0;33m X X X");//Yellow Colour
+    printf("\033[0;30m X X X X X X X");//Black Colour
+    printf("\033[0;32m X X\n");//Green Colour
+
+    printf("\033[0;32mX X X");//Green Colour
+    printf("\033[0;30m X"); //Black Colour
+    printf("\033[0;33m X X");//Yellow Colour
+    printf("\033[0;30m X");//Black Colour
+    printf("\033[0;37m X X X X X X X"); 
+    printf("\033[0;30m X"); //Black Colour
+    printf("\033[0;32m X\n"); //Green Colour
+
+    printf("\033[0;32mX X X"); //Green Colour
+    printf("\033[0;30m X"); //Black Colour
+    printf("\033[0;33m X"); //Yellow Colour
+    printf("\033[0;30m X"); //Black Colour
+    printf("\033[0;37m X X X X X X X X X"); //White Colour
+    printf("\033[0;30m X\n"); //Black Colour
+
+    printf("\033[0;32mX"); //Green Colour
+    printf("\033[0;30m X X X"); //Black Colour
+    printf("\033[0;33m X"); //Yellow Colour
+    printf("\033[0;30m X"); //Black Colour
+    printf("\033[0;37m X X X X X X X X X"); //White Colour
+    printf("\033[0;30m X\n"); //Black Colour
+
+    printf("\033[0;30mX"); //Black Colour
+    printf("\033[0;33m X X"); //Yellow Colour
+    printf("\033[0;30m X"); //Black Colour
+    printf("\033[0;33m X"); //Yellow Colour
+    printf("\033[0;30m X"); //Black Colour
+    printf("\033[0;37m X X X X X X X X X"); //White Colour
+    printf("\033[0;30m X\n"); //Black Colour
+
+    printf("\033[0;30mX"); //Black Colour
+    printf("\033[0;33m X X"); //Yellow Colour
+    printf("\033[0;30m X"); //Black Colour
+    printf("\033[0;33m X X"); //Yellow Colour
+    printf("\033[0;30m X"); //Black Colour
+    printf("\033[0;37m X X X X X X X"); //White Colour
+    printf("\033[0;30m X"); //Black Colour
+    printf("\033[0;32m X\n"); //Green Colour
+
+    printf("\033[0;30mX"); //Black Colour
+    printf("\033[0;33m X X"); //Yellow Colour
+    printf("\033[0;30m X"); //Black Colour
+    printf("\033[0;33m X X X"); //Yellow Colour
+    printf("\033[0;30m X X X X X X X X");//Black Colour
+    printf("\033[0;32m X\n");//Green Colour
+
+    printf("\033[0;30mX"); //Black Colour
+    printf("\033[0;33m X X"); //Yellow Colour
+    printf("\033[0;30m X"); //Black Colour
+    printf("\033[0;33m X X X M A D E X X X"); //Yellow Colour
+    printf("\033[0;30m X"); //Black Colour
+    printf("\033[0;32m X\n"); //Green Colour
+
+    printf("\033[0;30mX"); //Black Colour
+    printf("\033[0;33m X X"); //Yellow Colour
+    printf("\033[0;30m X"); //Black Colour
+    printf("\033[0;33m X X X X B Y X X X X"); //Yellow Colour
+    printf("\033[0;30m X"); //Black Colour
+    printf("\033[0;32m X\n"); //Green Colour
+
+    printf("\033[0;30mX"); //Black Colour
+    printf("\033[0;33m X X"); //Yellow Colour
+    printf("\033[0;30m X"); //Black Colour
+    printf("\033[0;33m X X S o h a m G X X"); //Yellow Colour
+    printf("\033[0;30m X"); //Black Colour
+    printf("\033[0;32m X\n"); //Green Colour
+
+    printf("\033[0;30mX"); //Black Colour
+    printf("\033[0;33m X X"); //Yellow Colour
+    printf("\033[0;30m X"); //Black Colour
+    printf("\033[0;33m X X X X X X X X X X"); //Yellow Colour
+    printf("\033[0;30m X"); //Black Colour
+    printf("\033[0;32m X\n"); //Green Colour
+
+    printf("\033[0;32mX"); //Green Colour
+    printf("\033[0;30m X X X"); //Black Colour
+    printf("\033[0;33m X X X X X X X X X X"); //Yellow Colour
+    printf("\033[0;30m X"); //Black Colour
+    printf("\033[0;32m X\n"); //Green Colour
+
+    printf("\033[0;32mX X X "); //Green Colour
+    printf("\033[0;30mX"); //Black Colour
+    printf("\033[0;33m X X X"); //Yellow Colour
+    printf("\033[0;30m X X X X"); //Black Colour
+    printf("\033[0;33m X X X"); //Yellow Colour
+    printf("\033[0;30m X"); //Black Colour
+    printf("\033[0;32m X\n"); //Green Colour
+
+    printf("\033[0;32mX X X"); //Green Colour
+    printf("\033[0;30m X"); //Black Colour
+    printf("\033[0;33m X X X"); //Yellow Colour
+    printf("\033[0;30m X"); //Black Colour
+    printf("\033[0;32m X X"); //Green Colour
+    printf("\033[0;30m X"); //Black Colour
+    printf("\033[0;33m X X X"); //Yellow Colour
+    printf("\033[0;30m X"); //Black Colour
+    printf("\033[0;32m X\n"); //Green Colour
+
+    printf("\033[0;32mX X X"); //Green Colour
+    printf("\033[0;30m X"); //Black Colour
+    printf("\033[0;33m X X X"); //Yellow Colour
+    printf("\033[0;30m X"); //Black Colour
+    printf("\033[0;32m X X"); //Green Colour
+    printf("\033[0;30m X"); //Black Colour
+    printf("\033[0;33m X X X"); //Yellow Colour
+    printf("\033[0;30m X"); //Black Colour
+    printf("\033[0;32m X\n"); //Green Colour
+
+    printf("\033[0;32mX X X X"); //Green Colour
+    printf("\033[0;30m X X X"); //Black Colour
+    printf("\033[0;32m X X X X"); //Green Colour
+    printf("\033[0;30m X X X"); //Black Colour
+    printf("\033[0;32m X X\n"); //Green Colour
+    
+    printf("\033[0;32mX X X X X X X X X X X X X X X X\n"); //Green Colour
+}
